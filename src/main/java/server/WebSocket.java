2b347e522ac40b540d36bfa6dfb559b75432d5d0
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-05-29 11:06:45 +0200

    Added websockets

---
+++ src/main/java/server/WebSocket.java
@@ -0,0 +1,47 @@
+package shared;
+
+import com.google.gson.Gson;
+import models.Answer;
+import server.GameSession;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ClientEndpoint
+@ServerEndpoint(value="/quizapp/")
+public class WebSocket
+{
+    @OnOpen
+    public void onWebSocketConnect(Session sess)
+    {
+        System.out.println("Socket Connected: " + sess);
+    }
+
+    @OnMessage
+    public void onWebSocketText(String message)
+    {
+        Gson gson = new Gson();
+        Answer answer = gson.fromJson(message, Answer.class);
+        GameSession game = new GameSession();
+        Boolean result = game.CheckAnswer(answer);
+        System.out.println("Received TEXT message: " + result);
+    }
+
+    @OnClose
+    public void onWebSocketClose(CloseReason reason)
+    {
+        System.out.println("Socket Closed: " + reason);
+    }
+
+    @OnError
+    public void onWebSocketError(Throwable cause)
+    {
+        cause.printStackTrace(System.err);
+    }
+}
